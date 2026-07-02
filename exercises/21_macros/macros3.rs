09380d9f336ae8581c6369a9bfeed3b2b32e0e37
ajit <azit.ghising@gmail.com>
2026-07-03 00:41:01 +0930

    All the macros solutions are complete.

--- exercises/21_macros/macros3.rs
+++ exercises/21_macros/macros3.rs
@@ -1,5 +1,17 @@
-// TODO: Fix the compiler error without taking the macro definition out of this
+
+
+
+
+
+
+
+
+
+
+
+// TODO: Fix the compiler error without taking the macro definition out of this 
 // module.
+#[macro_use]
 mod macros {
     macro_rules! my_macro {
         () => {
